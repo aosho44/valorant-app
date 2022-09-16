2b78d3e70a7f994bcd2651543cfbe87b7821b202
Ayo Osho <aosho44@gmail.com>
2022-09-16 12:52:41 +0100

    Add links for buttons

--- src/components/pages/Header/Header.jsx
+++ src/components/pages/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
 	return (
 		<div>
-			<button>Home</button>
-			<button>Agents</button>
+			<Link to={`/`}>
+				<button>Home</button>
+			</Link>
+			<Link to={`/agents`}>
+				<button>Agents</button>
+			</Link>
 		</div>
 	);
 }
