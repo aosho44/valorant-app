36a6f6fb1082288c8ebb95c7420aaca4199820c9
Ayo Osho <aosho44@gmail.com>
2022-09-16 06:34:07 +0100

    Update app.js and index.js with routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import AllAgentsPage from './components/pages/Characters/AllAgentsPage';
+import Footer from './components/pages/Footer/Footer';
+import Header from './components/pages/Header/Header';
+import Home from './components/pages/Home/Home';
+import PageNotFound from './components/pages/PageNotFound/PageNotFound';
+
 function App() {
-	return <div className="App">Valorant App</div>;
+	return (
+		<div className="app">
+			<Header></Header>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/agents" element={<AllAgentsPage />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
