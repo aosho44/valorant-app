2c90ef400b5c7440164b0af4337abb2019eca6c4
Ayo Osho <aosho44@gmail.com>
2022-09-16 06:32:43 +0100

    Add headers and footers page

---
+++ src/components/pages/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<div>
+			<h1>Created By Ayo</h1>
+		</div>
+	);
+}
+
+export default Footer;
---
+++ src/components/pages/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<div>
+			<button>Home</button>
+			<button>Agents</button>
+		</div>
+	);
+}
+
+export default Header;
