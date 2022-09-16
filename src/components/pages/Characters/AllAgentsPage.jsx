d87c4abc588c0cbe352cd4bf24ed2cf8210f89fd
Ayo Osho <aosho44@gmail.com>
2022-09-16 07:03:57 +0100

    Update to show fetched api data

--- src/components/pages/Characters/AllAgentsPage.jsx
+++ src/components/pages/Characters/AllAgentsPage.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Agents from './Agents';
+import { useDispatch } from 'react-redux';
+import { fetchAsyncAgents } from '../../features/agents/agentSlice';
 
 function AllAgentsPage() {
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchAsyncAgents());
+	}, [dispatch]);
 	return (
 		<div>
 			<Agents />
