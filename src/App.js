da07981d6f8513fe4bcdf19d6bd432e556e99a89
Ayo Osho <aosho44@gmail.com>
2022-09-16 11:52:20 +0100

    Change name of file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import AllAgentsPage from './components/pages/Characters/AllAgentsPage';
+import SingleAgentsView from './components/pages/Characters/SingleAgentsView';
 import Footer from './components/pages/Footer/Footer';
 import Header from './components/pages/Header/Header';
 import Home from './components/pages/Home/Home';
@@ -12,6 +13,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/agents" element={<AllAgentsPage />} />
+				<Route path="/agents/:id" element={<SingleAgentsView />} />
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 			<Footer />
